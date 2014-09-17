d92d4018b504ebbb752491978a2aeed2d79d242f
Daniel Petkevich <daniel.petkevich@tcc-danielpetk.local>
2014-09-16 23:02:07 -0700

    edited web.js

--- web.js
+++ web.js
@@ -1,8 +1,11 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var connect = require('connect')
+var logger = require('logger')
 var app = express();
+
  
-app.use(express.logger('dev'));
+
+app.use(logger);
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
